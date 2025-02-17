4c57f2b963f1c2d6da3114fc1dbc89d6ee309fd8
Felipe Kock Mendonça <info2.kock@gmail.com>
2025-02-17 16:58:18 -0300

    Finalizei o projeto

---
+++ back-end/api/server.js
@@ -0,0 +1,31 @@
+// API significa Application Programing Interface
+// POST, GET, PUT, DELETE
+// CRUD - CREATE, READ, UPDATE, DELETE
+// Endpoint
+// Middleware
+
+import express from "express";
+import cors from "cors";
+import { db } from "./connect.js";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+// app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("Só vamos trablhar com os endpoints '/artists' e 'songs'");
+});
+
+app.get("/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
+});
+
+app.get("/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor está escutando na porta ${PORT}`);
+});
